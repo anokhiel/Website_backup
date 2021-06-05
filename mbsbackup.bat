2c29f7d88c41e65f13d139696b63f9c110329b85
Alexander Popov <alexandpopov@yandex.ru>
2021-06-05 10:14:33 +0300

    Comments added

--- mbsbackup.bat
+++ mbsbackup.bat
@@ -1,19 +1,19 @@
 Setlocal 
-# Выясняем день недели
-For /f %%# In ('WMIC Path Win32_LocalTime Get DayOfWeek^|Findstr [0-6]') Do ( 
-        Set DOW=%%#)
+rem Выясняем день недели
+For /f %%rem In ('WMIC Path Win32_LocalTime Get DayOfWeek^|Findstr [0-6]') Do ( 
+        Set DOW=%%rem)
 cd "Место расположения текущей папки"
-# Надо откорректировать путь к putty, адрес и пароль
+rem Надо откорректировать путь к putty, адрес и пароль
 C:\putty\putty.exe -ssh root@128.0.0.1 -pw password -m backup.txt
-# Скачиваем архив в текущую папку
+rem Скачиваем архив в текущую папку
 winscp.com /command "open ftp://user:password@128.0.0.1" "get /backups/*.tar.gz" "exit"
-# По воскресениям загружаем последний архив на долговременное хранение
+rem По воскресениям загружаем последний архив на долговременное хранение
 if %DOW%==0 copy c:\buckups\*.tar.gz c:\buckups\archive\
-# Стираем предыдущий архив
+rem Стираем предыдущий архив
 del c:\buckups\*.tar.gz
-# Копируем скачанный архив в папку для архивов
+rem Копируем скачанный архив в папку для архивов
 copy  *.tar.gz c:\buckups\
-# Стираем скачанный архив из текущей папки
+rem Стираем скачанный архив из текущей папки
 del *.tar.gz
-# Стираем архив на сервере
+rem Стираем архив на сервере
 C:\putty\putty.exe -ssh root@128.0.0.1 -pw password -m backup1.txt
